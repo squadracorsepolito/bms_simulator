4ac856c985ec5695b590b824ada0628006d49f9d
carolesadek <carole.sadek@hotmail.com>
2025-05-19 20:27:36 +0200

    fix: remove build folder

--- build/hvcb.d
+++
@@ -1,3 +0,0 @@
-build/hvcb.o: SCan/SC24/artifacts/HVCB/c_source/hvcb.c \
- SCan/SC24/artifacts/HVCB/c_source/hvcb.h
-SCan/SC24/artifacts/HVCB/c_source/hvcb.h:
--- build/mcb.d
+++
@@ -1,3 +0,0 @@
-build/mcb.o: SCan/SC24/artifacts/MCB/c_source/Src/mcb.c \
- SCan/SC24/artifacts/MCB/c_source/Inc/mcb.h
-SCan/SC24/artifacts/MCB/c_source/Inc/mcb.h:
